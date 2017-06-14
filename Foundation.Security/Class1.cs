a438cb64ee64606c1cb7d826ea5d55519f4b2c95
Ritesh Ranjan <rranj7@NOIRRANJ762977>
2017-06-14 09:44:08 +0530

    Add project files.

---
+++ Foundation.Security/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foundation.Security
+{
+    public class Class1
+    {
+    }
+}
